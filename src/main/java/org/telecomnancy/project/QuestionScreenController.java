6842098c766ed9d69bdde6a181b46228314cc546
LucasEBSkora <skora.lucas@gmail.com>
2024-02-15 17:06:15 -0300

    adds skeleton of question page

---
+++ src/main/java/org/telecomnancy/project/QuestionScreenController.java
@@ -0,0 +1,21 @@
+package org.telecomnancy.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class QuestionScreenController {
+    @FXML
+    private Label questionField;
+
+    @FXML
+    protected void onReturnButtonClick() {
+
+    }
+    @FXML
+    protected void toLeft() {
+    }
+
+    @FXML
+    protected void toRight() {
+    }
+}
